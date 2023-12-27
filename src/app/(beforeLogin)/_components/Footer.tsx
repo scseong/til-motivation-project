da2a65396d877580531e4a2f9d98c208e5eb4e8f
nuny0831 <yrch92@gmail.com>
2023-12-27 17:54:20 +0900

    style: Footer UI 및 폴더 연결 (#2)

---
+++ src/app/(beforeLogin)/_components/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from './footer.module.scss';
+
+export default function Footer() {
+  return (
+    <div className={styles.footerBox}>
+      Copyright 2023 &copy; <strong>갓제너럴창성조 스윗챌린저 당도100% 이빨 다 썩음 당뇨걸릴뻔 인슐린 맞을 준비 하시조</strong> All rights reserved.
+    </div>
+  )
+}
--- src/app/(beforeLogin)/home/page.tsx
+++ src/app/(beforeLogin)/home/page.tsx
@@ -1,6 +1,6 @@
 import styles from './page.module.scss';
 
 export default function Home() {
-  return <div className={styles.container}>Home</div>;
+  return <div className={styles.container}>홈입니당</div>;
 }
 
