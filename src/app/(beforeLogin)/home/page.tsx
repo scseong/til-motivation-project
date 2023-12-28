4c41fb5c578372f253b7a809c8f635b3885d5e23
nuny0831 <yrch92@gmail.com>
2023-12-29 00:41:42 +0900

    feat: 모달 및 반응형 구현 (#15)

--- src/app/(beforeLogin)/home/page.tsx
+++ src/app/(beforeLogin)/home/page.tsx
@@ -1,6 +1,6 @@
+import Write from './_components/WriteBar';
 import List from './_components/List';
 import SideBar from './_components/SideBar';
-import Write from './_components/WriteBar';
 import styles from './page.module.scss';
 
 export default function Home() {
