910ce33791b098adad70215e8d5a313ef179e581
scseong <goidtv@naver.com>
2023-12-28 14:32:14 +0900

    feat: 회원가입, 로그인 폼 상태 관리 (#11)

--- src/app/(beforeLogin)/auth/login/page.tsx
+++ src/app/(beforeLogin)/auth/login/page.tsx
@@ -2,27 +2,39 @@
 import Link from 'next/link';
 import styles from './login.module.scss';
 import { FaGoogle, FaGithub } from 'react-icons/fa';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface LoginFormInput {
+  email: string;
+  password: string;
+}
 
 export default function Page() {
-  const handleSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault();
+  const { register, handleSubmit } = useForm<LoginFormInput>();
+  const onSubmit: SubmitHandler<LoginFormInput> = (data) => {
+    console.log(data);
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.login}>
         <h1>로그인</h1>
-        <form className={styles.form} onSubmit={handleSubmit}>
+        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
           <div className={styles.inputBox}>
             <label htmlFor="email">이메일</label>
-            <input id="email" type="email" placeholder="이메일 입력" />
+            <input {...register('email')} id="email" type="email" placeholder="이메일 입력" />
           </div>
           <div className={styles.inputBox}>
             <label htmlFor="password">비밀번호</label>
-            <input id="password" type="current-password" placeholder="비밀번호 입력" />
+            <input
+              {...register('password')}
+              id="password"
+              type="password"
+              placeholder="비밀번호 입력"
+            />
           </div>
           <div className={styles.btnBox}>
-            <button>이메일로 로그인</button>
+            <button type="submit">이메일로 로그인</button>
           </div>
           <div className={styles.divide}>
             <span>또는</span>
--- src/app/(beforeLogin)/auth/signup/page.tsx
+++ src/app/(beforeLogin)/auth/signup/page.tsx
@@ -1,36 +1,61 @@
 'use client';
 import Link from 'next/link';
 import styles from './signup.module.scss';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+interface SignUpInput {
+  email: string;
+  nickname: string;
+  password: string;
+  passwordCheck: string;
+  blogUrl: string;
+}
 
 export default function Page() {
-  const handleSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault();
+  const { register, handleSubmit } = useForm<SignUpInput>();
+  const onSubmit: SubmitHandler<SignUpInput> = (data) => {
+    console.log(data);
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.login}>
         <h1>회원가입</h1>
-        <form className={styles.form} onSubmit={handleSubmit}>
+        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
           <div className={styles.inputBox}>
             <label htmlFor="email">이메일</label>
-            <input id="email" type="email" placeholder="이메일 입력" />
+            <input {...register('email')} id="email" type="email" placeholder="이메일 입력" />
           </div>
           <div className={styles.inputBox}>
             <label htmlFor="nickname">닉네임</label>
-            <input id="nickname" type="text" placeholder="닉네임 입력" />
+            <input {...register('nickname')} id="nickname" type="text" placeholder="닉네임 입력" />
           </div>
           <div className={styles.inputBox}>
             <label htmlFor="password">비밀번호</label>
-            <input id="password" type="current-password" placeholder="비밀번호 입력" />
+            <input
+              {...register('password')}
+              id="password"
+              type="password"
+              placeholder="비밀번호 입력"
+            />
           </div>
           <div className={styles.inputBox}>
             <label htmlFor="password-check">비밀번호 확인</label>
-            <input id="password-check" type="current-password" placeholder="비밀번호 확인" />
+            <input
+              {...register('passwordCheck')}
+              id="password-check"
+              type="password"
+              placeholder="비밀번호 확인"
+            />
           </div>
           <div className={styles.inputBox}>
             <label htmlFor="blog-url">블로그 주소</label>
-            <input id="blog-url" type="url" placeholder="TIL 블로그 주소" />
+            <input
+              {...register('blogUrl')}
+              id="blog-url"
+              type="url"
+              placeholder="TIL 블로그 주소"
+            />
           </div>
           <div className={styles.btnBox}>
             <button>회원가입</button>
