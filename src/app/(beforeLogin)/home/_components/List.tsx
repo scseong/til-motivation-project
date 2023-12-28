a5b87f0004dd359247766d16af6292de2c75a3d0
nuny0831 <yrch92@gmail.com>
2023-12-28 16:05:28 +0900

    style: Main UI (#2)

--- src/app/(beforeLogin)/home/_components/List.tsx
+++ src/app/(beforeLogin)/home/_components/List.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import styles from './list.module.scss';
 import mockAvatar from '/public/images/logo.png';
+import { AiOutlineLike, AiOutlineShareAlt } from 'react-icons/ai';
+import { LiaCommentDots } from 'react-icons/lia';
 
 export default function List() {
   return (
@@ -17,53 +19,29 @@ export default function List() {
             </div>
             <button className={styles.postFollow}>팔로우</button>
           </div>
+          <div className={styles.postTitle}>데이터가 없을 때 200인가 404인가?</div>
           <div className={styles.postContent}>
-            <span>내용</span>
+            내용이 많은면 오또카지내용이 많은면 오또카지내용이 많은면 오또카지내용이 많은면
+            오또카지내용이 많은면 오또카지
           </div>
-        </div>
-        <div className={styles.post}>
-          <div className={styles.postHeader}>
-            <div className={styles.userBox}>
-              <Image className={styles.avatar} src={mockAvatar} alt="avatar" />
-              <div>
-                <div className={styles.postName}>닉네임</div>
-                <div className={styles.postDate}>12월25일</div>
+          <p>... 더 보기</p>
+          <div className={styles.openGraphBox}>오픈그래프자리</div>
+          <div className={styles.postFooter}>
+            <div>
+              <div className={styles.postLike}>
+                <AiOutlineLike size={18} />
+                <span>좋아요 </span>
+                <div className={styles.postLikeCount}>2</div>
+              </div>
+              <div className={styles.postComment}>
+                <LiaCommentDots size={18} />
+                <span>댓글 </span>
+                <div className={styles.postLikeCount}>2</div>
               </div>
             </div>
-            <button className={styles.postFollow}>팔로우</button>
-          </div>
-          <div className={styles.postContent}>
-            <span>내용</span>
-          </div>
-        </div>
-        <div className={styles.post}>
-          <div className={styles.postHeader}>
-            <div className={styles.userBox}>
-              <Image className={styles.avatar} src={mockAvatar} alt="avatar" />
-              <div>
-                <div className={styles.postName}>닉네임</div>
-                <div className={styles.postDate}>12월25일</div>
-              </div>
+            <div className={styles.postShare}>
+              <AiOutlineShareAlt size={18} />
             </div>
-            <button className={styles.postFollow}>팔로우</button>
-          </div>
-          <div className={styles.postContent}>
-            <span>내용</span>
-          </div>
-        </div>
-        <div className={styles.post}>
-          <div className={styles.postHeader}>
-            <div className={styles.userBox}>
-              <Image className={styles.avatar} src={mockAvatar} alt="avatar" />
-              <div>
-                <div className={styles.postName}>닉네임</div>
-                <div className={styles.postDate}>12월25일</div>
-              </div>
-            </div>
-            <button className={styles.postFollow}>팔로우</button>
-          </div>
-          <div className={styles.postContent}>
-            <span>내용</span>
           </div>
         </div>
       </div>
