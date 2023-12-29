ab160b4ff61854d43ce6d86889cfbff642cd3705
정유진 <jane320@naver.com>
2023-12-30 00:36:42 +0900

    fix: 충돌 해결 (#13)

--- src/app/(beforeLogin)/posts/[id]/page.tsx
+++ src/app/(beforeLogin)/posts/[id]/page.tsx
@@ -1,11 +1,16 @@
 import CommentList from './_components/CommentList';
 import Post from './_components/Post';
+import SideBar from '@/app/(beforeLogin)/home/_components/SideBar';
+import styles from './page.module.css';
 
 export default function Page() {
   return (
-    <>
-      <Post />
-      <CommentList/>
-    </>
+    <div className={styles.container}>
+      <div className={styles.right}>
+        <Post />
+        <CommentList />
+      </div>
+      <SideBar />
+    </div>
   );
 }
