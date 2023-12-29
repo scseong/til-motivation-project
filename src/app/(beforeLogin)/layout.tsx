d834a4daa54b9fb9c0b2b060b71ceb6fd7540b2d
nuny0831 <yrch92@gmail.com>
2023-12-29 11:33:48 +0900

    fix: layout 수정 (#19)

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -1,11 +1,9 @@
 import { ChildrenProp } from '@/typing/props';
-import NavBar from './_components/NavBar';
-import Footer from './_components/Footer';
 
 export default function Layout({ children }: ChildrenProp) {
   return <div>
-    <NavBar />
+\
     {children}
-    <Footer />
+
     </div>;
 }
