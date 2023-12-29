c1c107779d708a2df1387ea8512bbfbfe93e13de
정유진 <jane320@naver.com>
2023-12-30 00:43:02 +0900

    fix: 에러 확인 (#13)

--- src/app/(beforeLogin)/posts/[id]/_components/CommentList.tsx
+++ src/app/(beforeLogin)/posts/[id]/_components/CommentList.tsx
@@ -37,7 +37,7 @@ export default function CommnetList() {
               </Link>
             </div>
             <div className={styles.content}>
-              <p>{content}</p>
+              <p>{content}!</p>
             </div>
           </>
         );
