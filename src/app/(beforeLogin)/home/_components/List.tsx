78fc6b2c4bc48a262f140611ec69b1ee218120a8
nuny0831 <yrch92@gmail.com>
2023-12-30 11:46:30 +0900

    fix: 리스트 map 수정 (#23)

--- src/app/(beforeLogin)/home/_components/List.tsx
+++ src/app/(beforeLogin)/home/_components/List.tsx
@@ -21,55 +21,53 @@ export default function List() {
   return (
     <>
       <div className={styles.postBox}>
-        <div className={styles.post}>
-          {posts?.map((post, index) => (
-            <Fragment key={index}>
-              <Link href={`/posts/${post.psid}`}>
-                <div className={styles.postHeader}>
-                  <div className={styles.userBox}>
-                    <div className={styles.avatar}>
-                      <img src={post.photoURL} alt="아바타" />
-                    </div>
-                    <div>
-                      <div className={styles.postName}>{post.displayName}</div>
-                      <div className={styles.postDate}>
-                        {post.createdAt.toDate().toLocaleDateString('ko', {
-                          year: '2-digit',
-                          month: '2-digit',
-                          day: '2-digit'
-                        })}
-                      </div>
+        {posts?.map((post, index) => (
+          <div className={styles.post} key={index}>
+            <Link href={`/posts/${post.psid}`}>
+              <div className={styles.postHeader}>
+                <div className={styles.userBox}>
+                  <div className={styles.avatar}>
+                    <img src={post.photoUrl} alt="아바타" />
+                  </div>
+                  <div>
+                    <div className={styles.postName}>{post.displayName}</div>
+                    <div className={styles.postDate}>
+                      {post.createdAt.toDate().toLocaleDateString('ko', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric'
+                      })}
                     </div>
                   </div>
-                  <button className={styles.postFollow}>팔로우</button>
                 </div>
-                <div className={styles.postTitle}>{post.title}</div>
-                <div className={styles.postContent}>{post.content}</div>
-                <p>... 더 보기</p>
-                <div className={styles.openGraphBox}>오픈그래프자리</div>
-                <div className={styles.tag}>{post.tags.map((tag) => `#${tag} `)}</div>
-              </Link>
-              <div className={styles.postFooter}>
-                <div>
-                  <div className={styles.postLike}>
-                    <AiOutlineLike size={18} />
-                    <span>좋아요 </span>
-                    <div className={styles.postLikeCount}>{post.likesUser.length}</div>
-                  </div>
-                  <div className={styles.postComment}>
-                    <LiaCommentDots size={18} />
-                    {/* 댓글 구현시 댓글 수 표시 예정 */}
-                    <span>댓글 </span>
-                    <div className={styles.postLikeCount}>2</div>
-                  </div>
+                <button className={styles.postFollow}>팔로우</button>
+              </div>
+              <div className={styles.postTitle}>{post.title}</div>
+              <div className={styles.postContent}>{post.content}</div>
+              <p>... 더 보기</p>
+              <div className={styles.openGraphBox}>오픈그래프자리</div>
+              <div className={styles.tag}>{post.tags.map((tag) => `#${tag} `)}</div>
+            </Link>
+            <div className={styles.postFooter}>
+              <div>
+                <div className={styles.postLike}>
+                  <AiOutlineLike size={18} />
+                  <span>좋아요 </span>
+                  <div className={styles.postLikeCount}>{post.likesUser.length}</div>
                 </div>
-                <div className={styles.postShare}>
-                  <AiOutlineShareAlt size={18} />
+                <div className={styles.postComment}>
+                  <LiaCommentDots size={18} />
+                  {/* 댓글 구현시 댓글 수 표시 예정 */}
+                  <span>댓글 </span>
+                  <div className={styles.postLikeCount}>2</div>
                 </div>
               </div>
-            </Fragment>
-          ))}
-        </div>
+              <div className={styles.postShare}>
+                <AiOutlineShareAlt size={18} />
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
