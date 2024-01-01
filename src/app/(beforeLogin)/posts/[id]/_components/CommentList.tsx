c26fb2ba4d6d2221ea367f34cbaab25640f8c541
정유진 <jane320@naver.com>
2024-01-01 22:34:22 +0900

    feat : 댓글 수정하기 (#29)

--- src/app/(beforeLogin)/posts/[id]/_components/CommentList.tsx
+++ src/app/(beforeLogin)/posts/[id]/_components/CommentList.tsx
@@ -1,13 +1,18 @@
 'use client';
 import Link from 'next/link';
 import styles from './CommentList.module.scss';
+import Loader from '@/app/_components/Loader';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { deleteComment, getComments } from '@/shared/comment';
+import { deleteComment, getComments, updateComment } from '@/shared/comment';
 import { useParams } from 'next/navigation';
 import { FaTrashAlt } from 'react-icons/fa';
+import { useState } from 'react';
+import { Comment } from '@/typing/Post';
 
 export default function CommnetList() {
   const { id }: { id: string } = useParams();
+  const [editCommentId, setEditCommentId] = useState<string | null>(null);
+  const [text, setText] = useState('');
   const queryClient = useQueryClient();
   const {
     isLoading,
@@ -22,15 +27,32 @@ export default function CommnetList() {
     mutationFn: deleteComment,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['comments'] });
+      queryClient.invalidateQueries({ queryKey: ['post'] });
+      queryClient.invalidateQueries({ queryKey: ['posts'] });
     }
   });
-  const handleDelete = (commentId: string) => {
+  const handleDelete = (comment: Comment) => {
     if (window.confirm('댓글을 삭제하겠습니까?')) {
-      deleteCommentMutation.mutate(commentId);
+      deleteCommentMutation.mutate(comment);
     }
   };
 
-  if (isLoading) return <p>Loading...</p>;
+  const updateCommentMutation = useMutation({
+    mutationFn: updateComment,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['comments'] });
+      // queryClient.invalidateQueries({ queryKey: ['post'] });
+    }
+  });
+
+  const handleUpdate = (comment: Comment, text: string) => {
+    if (window.confirm('수정하시겠습니까?')) {
+      setEditCommentId(null);
+      updateCommentMutation.mutate({ comment, text });
+    }
+  };
+
+  if (isLoading) return <Loader />;
   if (error) return <p>{error.message}</p>;
 
   //const sortedComments = comments?.sort((a, b) => (b.createdAt > a.createdAt ? 1 : -1));
@@ -38,7 +60,7 @@ export default function CommnetList() {
   return (
     <div className={styles.layout}>
       {comments?.map((comment) => {
-        const { cid, displayName, content, photoUrl, createdAt } = comment;
+        const { cid, psid, displayName, content, photoUrl, createdAt } = comment;
         return (
           <>
             <div>
@@ -60,10 +82,21 @@ export default function CommnetList() {
               </Link>
             </div>
             <div className={styles.content}>
-              <p>{content}</p>
-              <button onClick={() => handleDelete(cid)}>
+              {editCommentId === cid ? (
+                <textarea defaultValue={content} onChange={(e) => setText(e.target.value)} />
+              ) : (
+                <p>{content}</p>
+              )}
+              <button onClick={() => handleDelete(comment)}>
                 <FaTrashAlt />
               </button>
+              {editCommentId !== cid && <button onClick={() => setEditCommentId(cid)}>수정</button>}
+              {editCommentId === cid && (
+                <button onClick={() => setEditCommentId(null)}>취소</button>
+              )}
+              {editCommentId === cid && (
+                <button onClick={() => handleUpdate(comment, text)}>수정 완료</button>
+              )}
             </div>
           </>
         );
--- src/shared/comment.ts
+++ src/shared/comment.ts
@@ -1,16 +1,6 @@
 import { Post } from '@/typing/Post';
 import { db } from './firebase';
-import {
-  doc,
-  getDoc,
-  updateDoc,
-  getDocs,
-  collection,
-  query,
-  where,
-  deleteDoc,
-  arrayUnion
-} from 'firebase/firestore';
+import { doc, getDoc, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore';
 import { Comment } from '@/typing/Post';
 
 export const getComments = async (postId: string) => {
@@ -18,21 +8,26 @@ export const getComments = async (postId: string) => {
   const docSnap = await getDoc(docRef);
   if (docSnap.exists()) {
     const comments = docSnap.data().comments as Comment[];
-    console.log('comments', comments);
     return comments.sort((a, b) => (b.createdAt > a.createdAt ? 1 : -1));
   }
 };
 
-export const deleteComment = async (commentId: string) => {
-  const deleted = query(collection(db, 'comments'), where('cid', '==', commentId));
-  const data = await getDocs(deleted);
-  return await deleteDoc(data.docs[0].ref);
+export const deleteComment = async (comment: Comment) => {
+  const postId = comment.psid;
+  const docRef = doc(db, 'posts', postId);
+  return await updateDoc(docRef, {
+    comments: arrayRemove(comment)
+  });
 };
 
 export const addComment = async (comment: Comment) => {
-  // const commentRef = doc(db, 'posts', comment.psid, 'comments', comment.cid);
-  // await setDoc(commentRef, comment);
-  await updateDoc(doc(db, 'posts', comment.psid), {
+  return await updateDoc(doc(db, 'posts', comment.psid), {
     comments: arrayUnion(comment)
   });
+};
+
+export const updateComment = async ({ comment, text }: { comment: Comment; text: string }) => {
+  deleteComment(comment);
+  const updated = { ...comment, content: text };
+  addComment(updated);
 };
