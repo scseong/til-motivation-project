b89cd1c756ff23dfb0c907411270ef3536cebf41
scseong <goidtv@naver.com>
2024-01-02 10:22:27 +0900

    fix: firebase admin 설정 에러 처리 (#54)

--- src/shared/firebase-admin.ts
+++ src/shared/firebase-admin.ts
@@ -2,7 +2,9 @@ import { initializeApp, getApps, cert } from 'firebase-admin/app';
 
 const firebaseAdminConfig = {
   credential: cert({
-    privateKey: `${process.env.NEXT_PUBLIC_PRIVATE_KEY}`.replace(/\\n/g, '\n'),
+    privateKey: process.env.NEXT_PUBLIC_PRIVATE_KEY
+      ? process.env.NEXT_PUBLIC_PRIVATE_KEY.replace(/\\n/g, '\n')
+      : undefined,
     clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
     projectId: process.env.NEXT_PUBLIC_PROJECT_ID
   })
