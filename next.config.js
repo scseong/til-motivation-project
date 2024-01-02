737588d846b7773c276ca67825334f6f91910622
coldbrew <minsuk815@naver.com>
2024-01-03 08:55:53 +0900

    feat: 프로필 수정(#81)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com'],
-    domains: ['firebasestorage.googleapis.com'],
+    domains: ['lh3.googleusercontent.com', 'firebasestorage.googleapis.com'],
   },
   async redirects() {
     return [
